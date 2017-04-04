267a6b077cf45c95c5fe29046cbe796d58a629c9
Nick-ch <nd.chernikov@gmail.com>
2017-04-04 13:03:18 +0300

    Работа над замечаниями

--- js/stat.js
+++ js/stat.js
@@ -14,13 +14,17 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура, вы победили!', 310, 40);
   ctx.fillText('Список результатов:', 310, 60);
 
-  var maxTime = 0;
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (maxTime < time) {
-      maxTime = time;
+// Возвращает наибольшое значение из массива arr
+  var maxTime = times[0];
+  function getMaxTime(arr) {
+    for (var i = 0; i < arr.length; i++) {
+      var currentTime = arr[i];
+      if (maxTime < currentTime) {
+        maxTime = currentTime;
+      }
     }
   }
+  getMaxTime(times);
 
   // Построение гистограмм
   /* Дано:
@@ -37,25 +41,23 @@ window.renderStatistics = function (ctx, names, times) {
   var histoStep = histoHeight / maxTime;
 
   function getRandomNumber(min, max) {
-    return (Math.random() * (max - min) + min).toFixed(3);
+    return (Math.random() * (max - min) + min);
   }
 
-  for (i = 0; i < names.length; i++) {
-    var name = names[i];
-    time = times [i];
+  for (var i = 0; i < names.length; i++) {
 
-    var height = histoStep * time;
+    var height = histoStep * times[i];
     var positionX = (histoBetween + histoWidth) * i;
 
-    if (name === 'Вы') {
+    if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + getRandomNumber(0.2, 1) + ')';
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + getRandomNumber(0.2, 1).toFixed(1) + ')';
     }
     ctx.fillRect(histoX + positionX, 250, histoWidth, -height);
     ctx.fillStyle = 'black';
     ctx.textAlign = 'left';
-    ctx.fillText(name, histoX + positionX, 260);
-    ctx.fillText(Math.round(time), histoX + positionX, (250 - height - 15));
+    ctx.fillText(names[i], histoX + positionX, 260);
+    ctx.fillText(Math.round(times[i]), histoX + positionX, (250 - height - 15));
   }
 };
