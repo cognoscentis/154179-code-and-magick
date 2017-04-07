82d4d484048b65b8fa03efb954e4c024fb55a875
Nick-ch <nd.chernikov@gmail.com>
2017-04-07 23:26:40 +0300

    Работа над замечаниями #2

--- js/setup.js
+++ js/setup.js
@@ -2,9 +2,9 @@
 
 var setupNode = document.querySelector('.setup');
 var setupSimilarNode = setupNode.querySelector('.setup-similar');
-var similarPersonList = document.querySelector('.setup-similar-list');
+var similarPersonList = setupNode.querySelector('.setup-similar-list');
 var similarPersonTemplate = document.getElementById('similar-wizard-template').content;
-var similarPersonsArray = [];
+var similarPersons = [];
 
 var WIZARD_NAMES = [
   'Иван',
@@ -44,30 +44,24 @@ var WIZARD_EYES_COLOR = [
 setupNode.classList.remove('hidden');
 setupSimilarNode.classList.remove('hidden');
 
-// Если не определить функцию генерации случайного числа в этом файле, то будет ругаться. Не понимаю, почему не берет эту же функцию из файла stat.js.
-// Он ведь раньше подключен.
 function getRandomNumber(min, max) {
   return (Math.random() * (max - min) + min);
 }
 
-function generateName() {
-  return WIZARD_NAMES[Math.floor(getRandomNumber(0, WIZARD_NAMES.length))] + ' ' + WIZARD_SURNAMES[Math.floor(getRandomNumber(0, WIZARD_SURNAMES.length))];
+function generateName(firstArray, secondArray) {
+  return firstArray[Math.floor(getRandomNumber(0, firstArray.length))] + ' ' + secondArray[Math.floor(getRandomNumber(0, secondArray.length))];
 }
 
-function getCoatColor() {
-  return WIZARD_COAT_COLOR[Math.floor(getRandomNumber(0, WIZARD_COAT_COLOR.length))];
-}
-
-function getEyesColor() {
-  return WIZARD_EYES_COLOR[Math.floor(getRandomNumber(0, WIZARD_EYES_COLOR.length))];
+function getRandomColor(array) {
+  return array[Math.floor(getRandomNumber(0, array.length))];
 }
 
 function createSimilarPersonsArray(array, count) {
   for (var i = 0; i < count; i++) {
     array.push({
-      name: generateName(),
-      coatColor: getCoatColor(),
-      eyesColor: getEyesColor()
+      name: generateName(WIZARD_NAMES, WIZARD_SURNAMES),
+      coatColor: getRandomColor(WIZARD_COAT_COLOR),
+      eyesColor: getRandomColor(WIZARD_EYES_COLOR)
     });
   }
   return array;
@@ -84,36 +78,10 @@ function createWizardTemplate(array, i) {
 function createDomWizardsList(count) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < count; i++) {
-    fragment.appendChild(createWizardTemplate(similarPersonsArray, i));
+    fragment.appendChild(createWizardTemplate(similarPersons, i));
   }
   similarPersonList.appendChild(fragment);
 }
 
-createSimilarPersonsArray(similarPersonsArray, 4);
+createSimilarPersonsArray(similarPersons, 4);
 createDomWizardsList(4);
-
-
-// Первоначальный вариант и комментарии к нему.
-// Проблема - верстка иногда съезжает от переполнения строки с именем. Пытаюсь решить это через JS - вставить тэг <br> между именем и фамилией. Не получается.
-// Наверное из-за того, что вставляю с помощью .textContent, а не .innerHTML.
-// .innerHTML тоже не срабатывает. А! Складывание со строкой приводит все элементы к строке.
-// нет, это не влияет. Пробовал вставлять через .innerHTML и без пробела ' ' - результат такой же. Хз что тут сделать можно
-// function createSimilarWizard() {
-//   var lineBreak = document.createElement('br');
-//   for (var i = 0; i < 4; i++) {
-//     var wizardElement = similarPersonTemplate.cloneNode(true);
-//     var similarPersonName = wizardElement.querySelector('.setup-similar-label');
-//     var randomName = WIZARD_NAMES[Math.floor(getRandomNumber(0, WIZARD_NAMES.length))];
-//     var randomSurname = WIZARD_SURNAMES[Math.floor(getRandomNumber(0, WIZARD_SURNAMES.length))];
-//     var randomCoatColor = WIZARD_COAT_COLOR[Math.floor(getRandomNumber(0, WIZARD_COAT_COLOR.length))];
-//     var randomEyeColor = WIZARD_EYES_COLOR[Math.floor(getRandomNumber(0, WIZARD_EYES_COLOR.length))];
-//
-//     similarPersonName.textContent = randomName + ' ' + similarPersonName.appendChild(lineBreak) + randomSurname;
-//     wizardElement.querySelector('.wizard-coat').style.fill = randomCoatColor;
-//     wizardElement.querySelector('.wizard-eyes').style.fill = randomEyeColor;
-//
-//     fragment.appendChild(wizardElement);
-//   }
-// }
-//
-// similarPersonList.appendChild(fragment);
