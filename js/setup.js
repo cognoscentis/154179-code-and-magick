248f7c762de9e24ee1f918836c931bf5ac81fc48
Nick-ch <nd.chernikov@gmail.com>
2017-04-08 12:06:58 +0300

    Работа над замчаниями #3

--- js/setup.js
+++ js/setup.js
@@ -6,6 +6,7 @@ var similarPersonList = setupNode.querySelector('.setup-similar-list');
 var similarPersonTemplate = document.getElementById('similar-wizard-template').content;
 var similarPersons = [];
 
+var SIMILAR_PERSON_COUNT = 4;
 var WIZARD_NAMES = [
   'Иван',
   'Хуан Себастьян',
@@ -44,34 +45,30 @@ var WIZARD_EYES_COLOR = [
 setupNode.classList.remove('hidden');
 setupSimilarNode.classList.remove('hidden');
 
-function getRandomNumber(min, max) {
-  return (Math.random() * (max - min) + min);
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
-function generateName(firstArray, secondArray) {
-  return firstArray[Math.floor(getRandomNumber(0, firstArray.length))] + ' ' + secondArray[Math.floor(getRandomNumber(0, secondArray.length))];
-}
-
-function getRandomColor(array) {
-  return array[Math.floor(getRandomNumber(0, array.length))];
+function getRandomElement(array) {
+  return array[(getRandomInt(0, array.length))];
 }
 
 function createSimilarPersonsArray(array, count) {
   for (var i = 0; i < count; i++) {
     array.push({
-      name: generateName(WIZARD_NAMES, WIZARD_SURNAMES),
-      coatColor: getRandomColor(WIZARD_COAT_COLOR),
-      eyesColor: getRandomColor(WIZARD_EYES_COLOR)
+      name: getRandomElement(WIZARD_NAMES) + ' ' + getRandomElement(WIZARD_SURNAMES),
+      coatColor: getRandomElement(WIZARD_COAT_COLOR),
+      eyesColor: getRandomElement(WIZARD_EYES_COLOR)
     });
   }
   return array;
 }
 
-function createWizardTemplate(array, i) {
+function createWizardTemplate(wizardData, i) {
   var wizardElement = similarPersonTemplate.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = array[i].name;
-  wizardElement.querySelector('.wizard-coat').style.fill = array[i].coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = array[i].eyesColor;
+  wizardElement.querySelector('.setup-similar-label').textContent = wizardData[i].name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizardData[i].coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizardData[i].eyesColor;
   return wizardElement;
 }
 
@@ -83,5 +80,5 @@ function createDomWizardsList(count) {
   similarPersonList.appendChild(fragment);
 }
 
-createSimilarPersonsArray(similarPersons, 4);
-createDomWizardsList(4);
+createSimilarPersonsArray(similarPersons, SIMILAR_PERSON_COUNT);
+createDomWizardsList(SIMILAR_PERSON_COUNT);
